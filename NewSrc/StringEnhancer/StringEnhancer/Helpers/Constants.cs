ad509dcd44f36306fbe51a90d7549d721689762a
Enache-Ionut <enachestoianionut@gmail.com>
2018-07-24 14:12:27 +0300

    Ignore already existing elements of String Tables that are not valid (that don't have IDs).

--- NewSrc/StringEnhancer/StringEnhancer/Helpers/Constants.cs
+++ NewSrc/StringEnhancer/StringEnhancer/Helpers/Constants.cs
@@ -6,5 +6,7 @@
     public const string kInvalidID = "___INVALID_ID";
     public const int kDuplicateID = -63;
     public const int kMaxID = 65535;
+    public const string kNotFoundID = "___NOT_FOUND_ID";
+    public const int kNotDiscovered = -1;
   }
 }
