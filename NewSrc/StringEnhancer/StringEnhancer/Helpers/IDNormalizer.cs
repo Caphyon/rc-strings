28e8f87c94cf800cd53696929886695e71234472
Enache-Ionut <enachestoianionut@gmail.com>
2018-07-31 14:54:05 +0300

    Extract functionalities from IDNormalizer into IDTrimmer + IDValidator

--- NewSrc/StringEnhancer/StringEnhancer/Helpers/IDNormalizer.cs
+++ NewSrc/StringEnhancer/StringEnhancer/Helpers/IDNormalizer.cs
@@ -1,48 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace StringEnhancer
 {
   public static class IDNormalizer
   {
-    public static string NormalizeID(string aID)
+    public static HeaderId NormalizeHexaID(HeaderId aID)
     {
-      if (aID.Length == 0)
-        return Constants.kInvalidID;
-
-      aID = aID.TrimEnd(new char[] { '\t', '/' });
-
-      if (IsHexaRepresentation(aID)) // Hexa case
-      {
-        aID = Convert.ToInt32(aID, 16).ToString();
-      }
-
-      var canParseToInt = int.TryParse(aID, out var idInt);
-      if (!canParseToInt || (idInt < 0 || idInt > Constants.kMaxID)) // ID is not in range [0, MaxID] or is invalid
-        return Constants.kInvalidID;
+      if (IDValidator.IsHexaRepresentation(aID))
+        aID.Value = Convert.ToInt32(aID.Value, 16).ToString();
+      
+      if (!IDValidator.IsValidInteger(aID))
+        aID = Constants.kInvalidID;
 
       return aID;
     }
 
-    public static string NormalizeReccurenceForID(string aID, Dictionary<string, string> aNameToID)
+    public static HeaderId NormalizeRecurrenceForID(HeaderId aID, HeaderContent aHeaderContent)
     {
-      while (!Regex.IsMatch(aID, @"^\d+$") && aNameToID.ContainsKey(aID)) // Recurrent case
-      {
-        aID = aNameToID[aID];
-      }
+      while (IDValidator.IsRecurrentCase(aID, aHeaderContent))
+        aID = aHeaderContent.NameToID[aID.Value];
 
-      if (!Regex.IsMatch(aID, @"^\d+$"))
-        return Constants.kInvalidID;
+      if (!IDValidator.IsValidWithoutRecurrenceCheck(aID))
+        aID = Constants.kInvalidID;
 
       return aID;
     }
-
-    public static bool IsHexaRepresentation(string aString) =>
-      aString.Length == 6 && aString.StartsWith("0x") && aString.Substring(2).All(IsHexaCharacter);
-
-    public static bool IsHexaCharacter(char aChar) =>
-      (aChar >= '0' && aChar <= '9') || (aChar >= 'A' && aChar <= 'F');
   }
 }
