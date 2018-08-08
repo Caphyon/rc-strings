81f79e24e2b0db999211aa476eb03669edaea27a
Enache-Ionut <enachestoianionut@gmail.com>
2018-08-08 10:56:46 +0300

    Fix warnings about overriding Equals() and GetHashCode() for HeaderId class

--- NewSrc/StringEnhancer/StringEnhancer/Header File/HeaderId.cs
+++ NewSrc/StringEnhancer/StringEnhancer/Header File/HeaderId.cs
@@ -1,4 +1,6 @@
-﻿namespace StringEnhancer
+﻿using System.Collections.Generic;
+
+namespace StringEnhancer
 {
   public class HeaderId
   {
@@ -27,7 +29,25 @@
 
     public bool IsHexa { get; set; }
 
-    #region Operator Overloading
+    #region Operator Overloading and overriding Equals() and GetHashCode()
+    public override bool Equals(object obj)
+    {
+      var id = obj as HeaderId;
+      return id != null &&
+             mValue == id.mValue &&
+             Value == id.Value &&
+             IsHexa == id.IsHexa;
+    }
+
+    public override int GetHashCode()
+    {
+      var hashCode = -409543468;
+      hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(mValue);
+      hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Value);
+      hashCode = hashCode * -1521134295 + IsHexa.GetHashCode();
+      return hashCode;
+    }
+
     public static bool operator ==(HeaderId first, HeaderId second) =>
       first?.Value == second?.Value;
     public static bool operator !=(HeaderId first, HeaderId second) =>
